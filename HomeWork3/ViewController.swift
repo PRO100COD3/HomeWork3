0b07ed2d217e2b57bba28a4fb8b7e0054c6d9262
PRO100COD3 <149900365+PRO100COD3@users.noreply.github.com>
2023-12-29 23:28:40 +0400

    Hard level of hw3

--- HomeWork3/ViewController.swift
+++ HomeWork3/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var delScoreButton: UIButton!
+    @IBOutlet private weak var scoreLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet private weak var infoTextView: UITextView!
+    @IBOutlet private weak var addScoreButton: UIButton!
+    
+    private var scoreCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scoreLabel.text = "Значение счетчика: " + String(scoreCount)
     }
-
-
+    @IBAction func addScore(_ sender: Any) {
+        scoreCount += 1
+        scoreLabel.text = "Значение счетчика: " + String(scoreCount)
+        let nowTime = Date().formatted()
+        infoTextView.text += ("\n" + nowTime + ": значение изменено на +1")
+    }
+    
+    @IBAction func resetScore(_ sender: Any) {
+        scoreCount = 0
+        scoreLabel.text = "Значение счетчика: " + String(scoreCount)
+        let nowTime = Date().formatted()
+        infoTextView.text += ("\n" + nowTime + ": значение сброшено")
+    }
+    @IBAction func delScore(_ sender: Any) {
+        
+        if(scoreCount <= 0){
+            let nowTime = Date().formatted()
+            infoTextView.text += ("\n" + nowTime + ": попытка уменьшить значение счётчика ниже 0")
+        }
+        else{
+            scoreCount -= 1
+            scoreLabel.text = "Значение счетчика: " + String(scoreCount)
+            let nowTime = Date().formatted()
+            infoTextView.text += ("\n" + nowTime + ": значение изменено на -1")
+        }
+    }
+    
 }
 
