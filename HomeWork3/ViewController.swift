488bdf1b533530859a2e979edcf76ac05cc2edf5
PRO100COD3 <149900365+PRO100COD3@users.noreply.github.com>
2024-01-04 20:18:30 +0400

    Fix small bug

--- HomeWork3/ViewController.swift
+++ HomeWork3/ViewController.swift
@@ -10,29 +10,28 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet private weak var delScoreButton: UIButton!
     @IBOutlet private weak var scoreLabel: UILabel!
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
     @IBOutlet private weak var infoTextView: UITextView!
     @IBOutlet private weak var addScoreButton: UIButton!
-    
     private var scoreCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         scoreLabel.text = "Значение счетчика: " + String(scoreCount)
     }
-    @IBAction func addScore(_ sender: Any) {
+    @IBAction private func addScore(_ sender: Any) {
         scoreCount += 1
         scoreLabel.text = "Значение счетчика: " + String(scoreCount)
         let nowTime = Date().formatted()
         infoTextView.text += ("\n" + nowTime + ": значение изменено на +1")
     }
     
-    @IBAction func resetScore(_ sender: Any) {
+    @IBAction private func resetScore(_ sender: Any) {
         scoreCount = 0
         scoreLabel.text = "Значение счетчика: " + String(scoreCount)
         let nowTime = Date().formatted()
         infoTextView.text += ("\n" + nowTime + ": значение сброшено")
     }
-    @IBAction func delScore(_ sender: Any) {
+    @IBAction private func delScore(_ sender: Any) {
         
         if(scoreCount <= 0){
             let nowTime = Date().formatted()
